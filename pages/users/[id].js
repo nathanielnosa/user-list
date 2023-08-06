bc91661cb02b3155b888ab0c43d94c266c7d211f
nathaniel nosa <nathanielnarth@gmail.com>
2023-08-06 08:17:01 +0100

    dynamic roue part one

--- pages/users/[id].js
+++ pages/users/[id].js
@@ -1,10 +1,11 @@
-const Text = () => {
+
+const Details = () => {
     return ( 
         <section id="test">
-            <h4>Test</h4>
+            <h4>Details Page</h4>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi suscipit distinctio aliquid eum temporibus atque quam quisquam libero laudantium est.</p>
         </section>
      );
 }
  
-export default Text;
+export default Details;
--- pages/users/index.js
+++ pages/users/index.js
@@ -2,13 +2,18 @@ import Link from "next/link"
 import style from "@/styles/User.module.css"
 export const getStaticProps = async ()=>{
   const res = await fetch('https://jsonplaceholder.typicode.com/users')
-  const data = await res.json()
+  if(!res){
+    alert('no connection to the userl')
+  }else{
 
-  return{
-    props:{
-      users:data
+    const data = await res.json()
+    return{
+      props:{
+        users:data
+      }
     }
   }
+
 }
 const index = ({users}) => {
   return (
@@ -18,7 +23,7 @@ const index = ({users}) => {
         {
           users.map((user)=>(
             <div key={user.id} className={style.single}>
-              <Link href={'/'} ><h3>{user.name}</h3></Link>
+              <Link href={'/users/'+user.id} ><h3>{user.name}</h3></Link>
             </div>
           ))
         }
