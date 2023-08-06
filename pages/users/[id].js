4a32208071c547123b1f45595da2098b9d828aff
nathaniel nosa <nathanielnarth@gmail.com>
2023-08-06 08:26:57 +0100

    dynamic routing to get detail page

--- pages/users/[id].js
+++ pages/users/[id].js
@@ -1,9 +1,24 @@
-
+export const getStaticPaths = async()=>{
+    const res = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await res.json();
+    const paths = data.map(user=>{
+        return {
+            params :{id:user.id.toString()}
+        }
+    })
+    return {
+        // paths: data.map(user=>`/users/${user.id}`),
+        paths,
+        fallback: false
+    }
+}
 const Details = () => {
     return ( 
         <section id="test">
             <h4>Details Page</h4>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi suscipit distinctio aliquid eum temporibus atque quam quisquam libero laudantium est.</p>
+            <div>
+                
+            </div>
         </section>
      );
 }
