62cc2d89621c2614acf9a0f97ba612a800af331a
nathaniel nosa <nathanielnarth@gmail.com>
2023-08-06 08:07:09 +0100

    fetching data

--- pages/users/index.js
+++ pages/users/index.js
@@ -1,9 +1,27 @@
+import Link from "next/link"
+import style from "@/styles/User.module.css"
+export const getStaticProps = async ()=>{
+  const res = await fetch('https://jsonplaceholder.typicode.com/users')
+  const data = await res.json()
 
-const index = () => {
+  return{
+    props:{
+      users:data
+    }
+  }
+}
+const index = ({users}) => {
   return (
     <section id="list">
         
-        <h3>List of users</h3>
+        <h3>List of Users</h3>
+        {
+          users.map((user)=>(
+            <div key={user.id} className={style.single}>
+              <Link href={'/'} ><h3>{user.name}</h3></Link>
+            </div>
+          ))
+        }
     </section>
   )
 }
